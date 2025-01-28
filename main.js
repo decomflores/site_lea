3482e8f6aa2317957739299fed1315b62dfbb648
decomflores <decomflores@gmail.com>
2025-01-28 11:29:23 -0300

    tentativa 2 de ajustar o erro do formulário

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ document.getElementById('interestForm').addEventListener('submit', async functio
     };
 
     try {
-        const response = await fetch('https://script.google.com/macros/s/AKfycbzcTTQCZLuMYUOjX0iTlajecslcaORrqMP_iJq55tr7q8RoDC3xKpU2OLqNlhBJf7sp0Q/exec', {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbzfAzkLXm966czbs6DF9-x9bzBAZ7QGmeYKERMVJE-NrF1uS0TGJN9MPpOJC34nS12fEA/exec', {
             method: 'POST',
             body: JSON.stringify(formData),
             headers: { 'Content-Type': 'application/json' },

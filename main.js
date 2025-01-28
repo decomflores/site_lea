62919f62ae9c4bad013a2c32ab9a57e7168d1569
decomflores <decomflores@gmail.com>
2025-01-28 11:16:31 -0300

    versão nova do site com formulário para cadastro de usuários

---
+++ main.js
@@ -0,0 +1,28 @@
+document.getElementById('interestForm').addEventListener('submit', async function (e) {
+    e.preventDefault();
+
+    const formData = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        phone: document.getElementById('phone').value,
+    };
+
+    try {
+        const response = await fetch('https://script.google.com/macros/s/AKfycbzcTTQCZLuMYUOjX0iTlajecslcaORrqMP_iJq55tr7q8RoDC3xKpU2OLqNlhBJf7sp0Q/exec', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            alert('Formulário enviado com sucesso!');
+            document.getElementById('interestForm').reset();
+            new bootstrap.Modal(document.getElementById('interestModal')).hide();
+        } else {
+            alert('Erro ao enviar o formulário. Tente novamente.');
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        alert('Erro ao enviar o formulário. Verifique sua conexão.');
+    }
+});
